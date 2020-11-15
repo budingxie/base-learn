b137967f2ac3f9167c4635eb9877bcd1fc7d160b
budingxie <3257632906@qq.com>
2020-11-15 18:06:55 +0800

    切片基本使用

--- chart01/array/arraystate.go
+++ chart01/array/arraystate.go
@@ -108,3 +108,138 @@ func UsePoint() {
 	*a1[0] = "Yellow"
 	fmt.Printf("a1[0]=%v, a2[0]=%v\n", *a1[0], *a2[0])
 }
+
+func MultiDimArr() {
+	// 1.申明一个二维整型数组，两个维度分别存储4个元素和2个元素
+	var a1 [4][2]int
+
+	// 2.使用数组字面量来申明并初始化一个二维数组
+	a2 := [4][2]int{{10, 11}, {20, 21}, {30, 31}, {40, 41}}
+
+	// 3.申明并初始化外层数组和内存数组的的单个元素
+	a3 := [4][2]int{1: {0: 20}, 3: {1: 41}}
+
+	fmt.Printf("a1: %v\na2:%v\na3: %v\n", a1, a2, a3)
+}
+
+func UseMultiDimArr() {
+	// 1.申明一个2 × 2的二维整型数组
+	var a1 [2][2]int
+
+	// 2.设置每个元素的整型值
+	a1[0][0] = 10
+	a1[0][1] = 20
+	a1[1][0] = 30
+	a1[1][1] = 40
+
+	// 3.只要类型一致，就可以将多维数组相互赋值
+	var a2 [2][2]int
+	a2 = a1
+
+	// 4.使用索引为多维数组赋值
+	var a3 [2]int = a1[1]
+	var value int = a1[1][0]
+	a1[1][0] = 100
+
+	fmt.Printf("a1:%v\na2:%v\na3:%v\nvalue:%v\n", a1, a2, a3, value)
+
+	// 当数组作为 方法的参数，最好使用 *[len]int，使用指针可以防止数组复制
+}
+
+func SliceCreate() {
+	// 1.创建一个字符串切片，其长度和容量都是5个元素
+	var slice1 []string = make([]string, 5)
+
+	// 2.创建一个整形切片，其长度为3给元素，容量为5个元素
+	slice2 := make([]int, 3, 5)
+
+	// 3.创建一个整型切片，使其长度大于容量。在编译的时候会报错
+	//slice3 := make([]int, 5, 3)
+	// len larger than cap in make([]int)
+
+	fmt.Printf("slice1:%v\nslice2:%v\n", slice1, slice2)
+}
+
+func UseSlice() {
+	// 1.创建字符串切片，其长度和容量都是5个元素
+	slice1 := []string{"Red", "Blue", "Green", "Yellow", "Pink"}
+
+	// 2.创建一个整型切片，其长度和容量都是3个元素
+	slice2 := []int{10, 120, 30}
+
+	// 3.创建字符串切片，使用1字符串初始化第100个元素
+	slice3 := []string{99: "1"}
+	fmt.Printf("slice1:%v\nslice2:%v\nslice3:%v\n", slice1, slice2, slice3)
+
+	// 4.nil和空切片
+	// 4.1创建nil整型切片
+	var slice4 []int
+
+	// 4.2使用make创建空的整型切片
+	slice5 := make([]int, 0)
+
+	// 4.3使用切片字面量创建空的整型切片
+	slice6 := []int{}
+
+	// 不管使用nil切片还是空切片，对其调用内置函数append、len和cap的效果都一样的
+	slice4 = append(slice4, 10)
+
+	fmt.Printf("slice4:%v\nslice5%v\nslice6:%v\n", slice4, slice5, slice6)
+
+	slice7 := []int{10, 20, 30, 40, 50}
+	slice7[1] = 25
+
+	fmt.Printf("slice7:%v\n", slice7)
+
+	slice8 := []int{10, 20, 30, 40, 50}
+	newSlice := slice8[1:3]
+	/*
+			slice8 := []int{10,20,30,40,50}
+			指针地址		长度5		容量5
+			    |
+				|
+		index:	0	1	2	3	4
+		value:	10	20	30	40	50
+					|
+				指针地址		长度2		容量4
+			newSlice :=slice[1:3]
+			对于newSlice，长度：3-1；容量：5-1
+	*/
+	// 切片slice8和newSlice 共用一个底层数组，当一个修改了值，另外的也同时变化了
+	newSlice[0] = 25
+	//newSlice[3] = 35 // index out of range [3] with length 2
+	fmt.Printf("slice8:%v\nnewSlice:%v\n", slice8, newSlice)
+
+	// 5.创建切片，并限制容量
+	source := []int{10, 20, 30, 40, 50}
+	// 从底层数组 index为2，长度为3-2， 容量为4-2
+	slice9 := source[2:3:4]
+	fmt.Printf("slice9:%v\nsource:%v\n", slice9, source)
+
+	slice9 = append(slice9, 45)
+	fmt.Printf("slice9:%v\nsource:%v\n", slice9, source)
+}
+
+func IterationSlice() {
+	slice := []int{10, 20, 30, 40}
+	for index, value := range slice {
+		fmt.Printf("Index: %d Value: %d\n", index, value)
+	}
+	// range创建了每个元素的副本，而不是直接返回对该元素的引用
+	for index, value := range slice {
+		fmt.Printf("Value: %d Value-Addr: %X ElemAddr: %X\n", value, &value, &slice[index])
+	}
+
+	for index := 2; index < len(slice); index++ {
+		fmt.Printf("Index: %d Value: %d\n", index, slice[index])
+	}
+
+	// 通过make创建
+	s1 := make([]int, 100)
+	s2 := make([]int, 0)
+	fmt.Printf("s1:%v\n", s1)
+	fmt.Printf("s2:%v\n", s2)
+
+	s2 = append(s2, 10)
+	fmt.Printf("s2:%v\n", s2)
+}
--- chart01/main.go
+++ chart01/main.go
@@ -17,4 +17,19 @@ func main() {
 
 	// 5.指针数组复制
 	array.UsePoint()
+
+	// 6.二维数组
+	array.MultiDimArr()
+
+	// 7.二维数组访问
+	array.UseMultiDimArr()
+
+	// 8.切片的创建
+	array.SliceCreate()
+
+	// 9.使用切片
+	array.UseSlice()
+
+	// 10.遍历切片
+	array.IterationSlice()
 }
